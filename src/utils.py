66d84784febdfa0ca2995874ad24dcc05e25ac9b
rballachay <riley.ballachay@gmail.com>
2022-01-17 19:52:22 -0500

    :bug: fix utils, update tests to pass

--- src/utils.py
+++ src/utils.py
@@ -16,7 +16,7 @@ class FileException(Exception):
 def process_config(inlet, file_num):
     xlsx = _get_xlsx(inlet)
     data_items={}
-    
+
     if str(file_num)=="1":
         data_items = _process_one_video(inlet,xlsx,data_items)
     elif str(file_num)=="2":
@@ -28,32 +28,29 @@ def process_config(inlet, file_num):
 
     return data_items
 
-
 def _process_two_videos(inlet,xlsx,data_items):
     for title in ["concentration","washing"]:
-        data_items = _process_one_video(inlet,xlsx,data_items,title=title,n_ideal=2)
+        data_items = _process_one_video(inlet,xlsx,data_items,title=title,n_total=2)
     return data_items
 
 
-def _process_one_video(inlet,xlsx,data_items,title="*",n_ideal=1):
+def _process_one_video(inlet,xlsx,data_items,title="total",n_total=1):
     n = _count_files(inlet)
-    if n>n_ideal:
+    if n>n_total:
         raise FileException("too many", "videos", inlet)
-    elif n<n_ideal:
+    elif n<n_total:
         raise FileException("too few", "videos", inlet)
     else:
-        video_name = _get_video_name(inlet, title)
-        vid_title = _get_video(inlet, video_name)
-        title = _check_file_naming(vid_title)
+        vid_title = _get_video_name(inlet, title)
         data = _make_data_dictionary(title, xlsx)
-        data_items[title] = {"video":vid_title, "data":data}
+        data_items[_check_file_naming(vid_title)] = {"video":vid_title, "data":data}
 
     return data_items
 
 
 def _get_video_name(path,title):
-    if title=="*":
-        return title
+    if title=="total":
+        return _get_video(path)[0]
     else:
         vids = _get_video(path)
         for vid in vids:
@@ -79,6 +76,7 @@ def _get_data(title, xlsx):
         stage,sensor = xl_title.split("_")
         if stage==title:
             sensor_data[sensor] = xlsx[xl_title]
+
     return sensor_data
 
 
@@ -89,7 +87,6 @@ def _check_file_naming(vid_title):
         return "washing"
     else:
         return "total"
-
 
 def _get_video(path, video_name="*.mp4"):
     return glob.glob(f"{path}{os.sep}{video_name}")
