52f42b7cfa3e22ec5b79073e0803d1053c7a6fd2
rballachay <riley.ballachay@gmail.com>
2022-01-29 14:06:46 -0500

    :art: change .env to work for windows

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 from typing import Dict
 
@@ -6,4 +7,9 @@ from dotenv import dotenv_values
 dotenv_path = ".env"
 
 config: Dict = dotenv_values(dotenv_path)
+
+for c in config:
+    if "/" in config[c]:
+        config[c] = f"{os.sep.join(config[c].split('/'))}"
+
 ENV = namedtuple("ENV", config)(**config)
