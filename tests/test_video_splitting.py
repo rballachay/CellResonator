282288500ae54064bb85294bde85b08ef192d04d
rballachay <riley.ballachay@gmail.com>
2022-01-31 19:29:24 -0500

    :white_check_mark: fix video splitting

--- tests/test_video_splitting.py
+++ tests/test_video_splitting.py
@@ -3,7 +3,7 @@ import os
 
 
 def test_total_video_splitter(cleaning, load_result_data):
-    json_path = f"tests/data{os.sep}sample_output{os.sep}result_total.json"
+    json_path = f"tests{os.sep}data{os.sep}sample_output{os.sep}result_total.json"
     data_dict = load_result_data(json_path, _key="total")
     total_video_splitter(data_dict)
     cleaning(f"tests{os.sep}data{os.sep}sample_output")
